da315b653d5e9284b42c3df18332755e6d572332
desireemezzotero <desireemezzotero1@gmail.com>
2025-10-19 14:37:01 +0200

    chore: project scaffolding and initial migration added

---
+++ database/migrations/2025_10_19_122857_create_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->integer('userid');
+            $table->id()->primary();
+            $table->string('title', 1024);
+            $table->string('body', 2048);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
